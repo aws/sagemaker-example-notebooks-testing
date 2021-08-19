6c93664559a8dc35ea1789385114db348e5934d9
Shreya Pandit <pandishr@amazon.com>
2021-08-19 16:05:07 -0700

    Update skip list

--- cdk-project/lib/images/codebuild-image/python/src/notebooks/parse.py
+++ cdk-project/lib/images/codebuild-image/python/src/notebooks/parse.py
@@ -14,6 +14,7 @@ SKIP_LIST = {
     "ingest_data",
     "label_data",
     "prep_data",
+    "async-inference/Async-Inference-Walkthrough.ipynb"
     "sagemaker-fundamentals",
     "sagemaker-pipelines/tabular/lambda-step/sagemaker-pipelines-lambda-step.ipynb",
     "sagemaker-pipelines/tabular/tensorflow2-california-housing-sagemaker-pipelines-deploy-endpoint/tensorflow2-california-housing-sagemaker-pipelines-deploy-endpoint.ipynb",
